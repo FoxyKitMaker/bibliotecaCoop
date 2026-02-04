1dd32f977795a12ef752474a0fa8b9bfef85ad26
ManuEnrique <mevargab01@iesalbarregas.es>
2026-02-04 19:50:55 +0100

    actualizacion usuario

--- biblioteca/src/GeneroLib.java
+++ biblioteca/src/GeneroLib.java
@@ -1,3 +1,9 @@
 public enum GeneroLib {
-    
+    NARRATIVA,
+    CIENCIA_FICCION,
+    FANTASIA,
+    POESIA,
+    TERROR,
+    TEATRO,
+    BIOGRAFIA
 }
--- biblioteca/src/Usuario.java
+++ biblioteca/src/Usuario.java
@@ -1,3 +1,37 @@
+/**@author Manuel Enrique Vargas Béjar
+ * @version 0.1a
+ * Funciones de Usuarios.
+ * 1.0 - Buscar libros por título, autor o categoría.
+ * 1.1 - Mostrar todos los libros disponibles
+ * 1.2 - Realizar préstamos de libros.
+ * 1.3 - Devolver libros prestados.
+ */
+
 public class Usuario {
-    
-}
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String idUsuario;
+
+    public Usuario(String nombre, String apellido, String dni, String idUsuario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.idUsuario = idUsuario;
+    }
+    public String getnombre(){
+        return nombre;
+    }
+    public String getapellido(){
+        return apellido;
+    }
+    public String getdni(){
+        return dni;
+    }
+    public String getidusuario(){
+        return idUsuario;
+    }
+
+    }
+        
+
