276dabdc1b882b9a6fc5461a68f066552f33b7aa
claudiogc-17 <claudio2015@iesalbarregas.es>
2026-02-05 20:49:27 +0100

    biblioteca

--- src/main/java/com/biblioteca/Biblioteca.java
+++ src/main/java/com/biblioteca/Biblioteca.java
@@ -1,10 +1,31 @@
 package com.biblioteca;
 
 /**
- * @author
+ * Representa una lógica para guardar los libros existentes y ofrece
+ * métodos para que otras clases puedan modificar o consultar el catálogo
+ * 
+ * @author Claudio García Camons
  * @version 0.1a
  */
 
 public class Biblioteca {
-    
+    private String nombre;
+    private Libros [] arrayLibros;
+    private int contadorLibros;
+
+    public Biblioteca(String nombre){
+        this.nombre = nombre;
+        this.arrayLibros = new Libros[100];
+        this.contadorLibros = 0;
+    }
+
+    public void agregarLibro(Libros libro){
+
+        if(i=contador)
+        for(int i=0; i<contadorLibros; i++){
+            if(arrayLibros[i]==null){
+                arrayLibros[i]=libro;
+            }
+        }
+    }
 }
