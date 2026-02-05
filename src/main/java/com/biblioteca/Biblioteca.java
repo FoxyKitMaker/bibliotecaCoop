d44835ec5393a0b0f07541bb03bccb76cddbba85
claudiogc-17 <claudio2015@iesalbarregas.es>
2026-02-05 21:27:23 +0100

    libros, usuarios,

--- src/main/java/com/biblioteca/Biblioteca.java
+++ src/main/java/com/biblioteca/Biblioteca.java
@@ -2,7 +2,8 @@ package com.biblioteca;
 
 /**
  * Representa una lógica para guardar los libros existentes y ofrece
- * métodos para que otras clases puedan modificar o consultar el catálogo
+ * métodos para que otras clases puedan modificar o consultar el catálogo.
+ * Tambien almacena los usuarios existentes
  * 
  * @author Claudio García Camons
  * @version 0.1a
@@ -11,21 +12,40 @@ package com.biblioteca;
 public class Biblioteca {
     private String nombre;
     private Libros [] arrayLibros;
+    private Usuario [] arrayUsuarios;
     private int contadorLibros;
 
     public Biblioteca(String nombre){
         this.nombre = nombre;
-        this.arrayLibros = new Libros[100];
-        this.contadorLibros = 0;
+        this.arrayLibros = new Libros[0];
+        this.arrayUsuarios = new Usuario[0];
     }
 
     public void agregarLibro(Libros libro){
 
-        if(i=contador)
-        for(int i=0; i<contadorLibros; i++){
-            if(arrayLibros[i]==null){
-                arrayLibros[i]=libro;
+        if(this.arrayLibros==null){
+            this.arrayLibros = new Libros[1];
+            this.arrayLibros[0] = libro;
+        }else{
+            Libros [] arrayNuevo = new Libros[this.arrayLibros.length+1];
+
+            for(int i=0; i<this.arrayLibros.length; i++){
+                arrayNuevo[i] = arrayLibros[i];
             }
+
+            arrayNuevo[arrayNuevo.length-1] = libro;
+            this.arrayLibros = arrayNuevo;
+        }
+
+    }
+
+    public void agregarUsuario(Usuario usuario){
+        if(this.arrayUsuarios==null){
+            this.arrayLibros = new Libros[1];
+            this.arrayUsuarios[0] = usuario;
+        }else{
+            Usuario [] arrayNuevo = new Usuario[this.arrayUsuarios.length+1];
+            
         }
     }
 }
